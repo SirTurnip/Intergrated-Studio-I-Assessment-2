234dbad7ba9ed9b99574fb2278ec4a7a61f1b256
Oebus <oliverw4321@gmail.com>
2022-06-16 12:43:43 +1200

    login start

--- main.cpp
+++ main.cpp
@@ -1,3 +1,16 @@
+#include <iostream>
+using namespace std;
+// School Managment App
 
 int main() {
+    // welcome user to app
+    cout << "Welcome to the School Management App" << endl;
+    cout << "Choose login option: " << endl;
+
+    cout << "1. Admin" << endl;
+    cout << "2. Teacher" << endl;
+    cout << "3. Parent" << endl;
+
+    int login_option;
+    cin >> login_option;
 }
